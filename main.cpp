315f83031c35242cb60a4564c7e65444095ce76d
legleb <sedovgleb2570@gmail.com>
2026-03-17 11:09:39 +0300

    add min test framework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include "top-it-vector.hpp"
 
-int main(int, char ** )
+bool testEmptyVector()
 {
+  return false;
+}
+
+int main()
+{
+  using test_t = std::pair< const char *, bool(*)() >;
+  test_t tests[] = {
+    { "Empty vector", testEmptyVector }
+  };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  for (size_t i = 0; i < count; ++i)
+  {
+    std::cout << tests[i].first << ": " << tests[i].second() << "\n";
+  }
 }
--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -1,10 +1,26 @@
 #ifndef TOP_IT_VECTOR_HPP
 #define TOP_IT_VECTOR_HPP
 #include <cstddef>
-template< class T >
-struct Vector
+namespace topit
 {
-  T* data;
-  size_t size, capacity;
-};
+  template< class T >
+  struct Vector
+  {
+    ~Vector();
+    Vector();
+
+  private:
+    T* data_;
+    size_t size_, capacity_;
+  };
+}
+
+template< class T >
+topit::Vector< T >::~Vector()
+{}
+
+template< class T >
+topit::Vector< T >::Vector():
+  data_(), size_(), capacity_()
+{}
 #endif
