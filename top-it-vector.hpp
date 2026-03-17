57221265f187c877b51cccdcc173a833073863bc
legleb <sedovgleb2570@gmail.com>
2026-03-17 11:33:17 +0300

    complete classwork

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -8,7 +8,19 @@ namespace topit
   {
     ~Vector();
     Vector();
-    bool isEmpty() const noexcept;
+    Vector(const Vector&);
+    Vector(Vector&&);
+    Vector& operator=(const Vector&);
+    Vector& operator=(Vector&&);
+
+    bool isEmpty() const noexcept; // доделать
+    size_t getSize() const noexcept; // доделать
+    size_t getCapacity() const noexcept; // доделать
+
+    void pushBack(const T& v); // доделать
+    void popBack(); // доделать
+    void insert(size_t i, const T& v);
+    void erase(size_t i);
 
   private:
     T* data_;
@@ -18,16 +30,24 @@ namespace topit
 
 template< class T >
 topit::Vector< T >::~Vector()
-{}
+{
+  delete[] data_;
+}
 
 template< class T >
 topit::Vector< T >::Vector():
-  data_(), size_(), capacity_()
+  data_(nullptr), size_(0), capacity_(0)
 {}
 
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return false;
+  return !size_;
+}
+
+template< class T >
+void topit::Vector< T >::pushBack(const T& v)
+{
+
 }
 #endif
